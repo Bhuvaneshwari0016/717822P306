9a8b31e6022dc98232b33e0c76a6b1410d07b5dc
Bhuvaneshwari0016 <bhuvana.pcm716@gmail.com>
2025-03-22 13:56:17 +0530

    Add files via upload

---
+++ calculator/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AvergeCalculator from "./components/AvergeCalculator";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <AvergeCalculator />
+    </div>
+  );
+}
+
+export default App;
